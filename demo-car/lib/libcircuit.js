c57cf6e1d44e535af2797e2f89466b6cf19ae4b3
Alberto López Sánchez <albertolopezsanchez9@gmail.com>
2015-12-29 20:45:12 +0100

    FIX: All of body's fixtures belongs to only one body, not to separate bodies. Also add name to bodies in userData property.

--- demo-car/lib/libcircuit.js
+++ demo-car/lib/libcircuit.js
@@ -2,43 +2,49 @@
 function createCircuit() {
 
     //===BORDERS===================================================================
-    var bodyDef = new b2BodyDef;
+    var bodyDef = new b2BodyDef();
     bodyDef.type = b2Body.b2_staticBody;
-    var fixDef = new b2FixtureDef;
-    fixDef.shape = new b2PolygonShape;
+    bodyDef.userData = "border";
+    var fixDef = new b2FixtureDef();
+    fixDef.shape = new b2PolygonShape();
 
     // lower border
+    fixDef.shape.SetAsBox(50, 0.01);
     bodyDef.position.Set(30, 60);
-    fixDef.shape.SetAsBox(50, .01);
     world.CreateBody(bodyDef).CreateFixture(fixDef);
 
     // top border
     bodyDef.position.Set(30, 0);
-    fixDef.shape.SetAsBox(50, .01);
     world.CreateBody(bodyDef).CreateFixture(fixDef);
 
     // right border
+    fixDef.shape.SetAsBox(0.01, 50);
     bodyDef.position.Set(75, 30);
-    fixDef.shape.SetAsBox(.01, 50);
     world.CreateBody(bodyDef).CreateFixture(fixDef);
 
     // left border
     bodyDef.position.Set(0, 30);
-    fixDef.shape.SetAsBox(.01, 50);
     world.CreateBody(bodyDef).CreateFixture(fixDef);
 
 
     // CREATE RESTRINGED AREAS ===================================================================
+    bodyDef = new b2BodyDef();
+    bodyDef.type = b2Body.b2_staticBody;
+    fixDef = new b2FixtureDef();
+
     bodyDef.position.Set(0, 0);
+    bodyDef.userData = "wall";
+    fixDef.shape = new b2PolygonShape();
 
     // top-left semi-circle
     var top_left = new Bezier(10, 20, 10, 10, 20, 10);
     var vecs = getVecsBezier(top_left);
     vecs.push(vecs[0]);
 
+    var wall = world.CreateBody(bodyDef);
     for (var i = 0; i < vecs.length - 1; i++) {
         fixDef.shape.SetAsEdge(vecs[i], vecs[i + 1]);
-        world.CreateBody(bodyDef).CreateFixture(fixDef);
+        wall.CreateFixture(fixDef);
     }
 
     // top-right moon
@@ -50,9 +56,10 @@ function createCircuit() {
         vecs.push(p);
     });
 
-    for (var i = 0; i < vecs.length - 1; i++) {
+    wall = world.CreateBody(bodyDef);
+    for (i = 0; i < vecs.length - 1; i++) {
         fixDef.shape.SetAsEdge(vecs[i], vecs[i + 1]);
-        world.CreateBody(bodyDef).CreateFixture(fixDef);
+        wall.CreateFixture(fixDef);
     }
 
     // lower bezier curves
@@ -60,14 +67,16 @@ function createCircuit() {
     vecs = [];
     vecs = getVecsBezier(lower_bezier);
 
-    for (var i = 0; i < vecs.length - 1; i++) {
+    wall = world.CreateBody(bodyDef);
+    for (i = 0; i < vecs.length - 1; i++) {
         fixDef.shape.SetAsEdge(vecs[i], vecs[i + 1]);
-        world.CreateBody(bodyDef).CreateFixture(fixDef);
+        wall.CreateFixture(fixDef);
     }
 
     // CREATE OBSTACLES ===================================================================
 
     bodyDef.type = b2Body.b2_dynamicBody;
+    bodyDef.userData = "obstacles";
     bodyDef.linearDamping = 1;
     bodyDef.angularDamping = 1;
     fixDef.shape = new b2CircleShape(1);
@@ -75,28 +84,49 @@ function createCircuit() {
     fixDef.friction = 50000;
     fixDef.restitution = 0.2;
     var xCircle = 0;
-    for (var i = 0; i < 5; i++) {
+    for (i=0; i < 5; i++) {
         xCircle += 10;
         bodyDef.position.Set(xCircle, 25);
         world.CreateBody(bodyDef).CreateFixture(fixDef);
     }
+
+    // CREATE LINES ====================================================================
+
+    vecs = [];
+    vecs = getVecsBezier(new Bezier(35,28,  35,20, 35,10));
+
+    bodyDef = new b2BodyDef();
+    bodyDef.position.Set(0,0);
+    bodyDef.type = b2Body.b2_dynamicBody;
+    bodyDef.userData = "line";
+    var mylines = world.CreateBody(bodyDef);
+
+    fixDef = new b2FixtureDef();
+    fixDef.shape = new b2PolygonShape();
+
+    var line = world.CreateBody(bodyDef);
+    for (i = 0; i < vecs.length - 1; i++) {
+        fixDef.shape.SetAsEdge(vecs[i], vecs[i + 1]);
+        mylines.CreateFixture(fixDef);
+    }
+
 
 
 
     // FUNCTIONS===================================================================
 
     function getVecsBezier(curve, step) {
-        var step = step | 100;
+        step = step | 100;
         var LUT = curve.getLUT();
         var vecs = [];
         LUT.forEach(function(p) {
             var vec = new b2Vec2();
             vec.Set(p.x, p.y);
             vecs.push(vec);
-        })
+        });
         return vecs;
     }
 
 
 
-};
+}
