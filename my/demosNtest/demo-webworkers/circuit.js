3b24bdc2419eb368f81030c7fd8543287771751b
Alberto López Sánchez <albertolopezsanchez9@gmail.com>
2016-01-17 20:29:34 +0100

    Added base to work with Robode and webworkers

---
+++ my/demosNtest/demo-webworkers/circuit.js
@@ -0,0 +1,38 @@
+function createCircuit() {
+
+    var scale = 10;
+    var canvasWidth = $('canvas').get(0).width;
+    var canvasHeight = $('canvas').get(0).height;
+
+    //===BORDERS===================================================================
+    var bodyDef = new b2BodyDef();
+    bodyDef.type = b2Body.b2_staticBody;
+    bodyDef.userData = createNewUserData("border");
+    var fixDef = new b2FixtureDef();
+    fixDef.shape = new b2PolygonShape();
+
+    // lower border
+    fixDef.shape.SetAsBox(canvasWidth/scale, 0.01);
+    bodyDef.position.Set((canvasWidth/scale)/2, canvasHeight/scale);
+    world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+    // top border
+    bodyDef.position.Set((canvasWidth/scale)/2, 0);
+    world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+    // right border
+    fixDef.shape.SetAsBox(0.01, canvasHeight/scale);
+    bodyDef.position.Set(canvasWidth/scale, (canvasHeight/scale)/2);
+    world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+    // left border
+    bodyDef.position.Set(0, (canvasHeight/scale)/2);
+    world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+
+
+    function createNewUserData(bodyType) {
+        return bodyType + Math.floor(Math.random() * 1000);
+    }
+
+}
