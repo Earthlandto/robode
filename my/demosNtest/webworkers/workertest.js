ba179c4a5d56e820d7fb4ab51032520c8a5bb032
Alberto López Sánchez <albertolopezsanchez9@gmail.com>
2016-01-23 00:54:31 +0100

    Fix minor errors in webworker demo

--- my/demosNtest/webworkers/workertest.js
+++ my/demosNtest/webworkers/workertest.js
@@ -2,4 +2,4 @@ self.onmessage = function(e) {
     console.log("worker: Received " + e.data);
 };
 
-self.postMessage('msg from worker to main thread ' + e.data);
+self.postMessage('msg from worker to main thread ');
